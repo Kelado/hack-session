ef73cee24d5c7fbaf8efb0d68d6c4cd23caeca80
kelado <kelantonag@gmail.com>
2026-01-21 15:27:37 +0200

    Step 3: Ping-Pong Responder - Listen for messages and respond to ping

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -27,6 +29,14 @@ func main() {
 		log.Fatalf("Error creating Discord session: %v", err)
 	}
 
+	// Register the message handler BEFORE opening the connection
+	dg.AddHandler(messageCreate)
+
+	// Set intents to receive guild messages and message content
+	// Note: You must enable "Message Content Intent" in the Discord Developer Portal
+	// (Bot tab -> Privileged Gateway Intents -> Message Content Intent)
+	dg.Identify.Intents = discordgo.IntentGuildMessages | discordgo.IntentMessageContent
+
 	err = dg.Open()
 	if err != nil {
 		log.Fatalf("Error opening connection: %v", err)
@@ -41,4 +51,26 @@ func main() {
 		log.Fatalf("Error sending message: %v", err)
 	}
 	fmt.Println("Message sent successfully!")
+	fmt.Println("Bot is now listening for messages. Press CTRL-C to exit.")
+
+	// Wait for a termination signal
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
+// messageCreate is called every time a new message is created in a channel the bot has access to
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore messages from the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Respond to "ping" with "pong"
+	if m.Content == "ping" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "pong 🏓")
+		if err != nil {
+			log.Printf("Error sending message: %v", err)
+		}
+	}
 }
